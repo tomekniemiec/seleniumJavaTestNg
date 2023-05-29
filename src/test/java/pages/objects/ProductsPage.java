3d7ffbf7863c263e68b5d91abc9df8bdf03f3d23
tomaszniemiec <tomek.niemiec@icloud.com>
2023-05-29 23:56:40 +0200

    add sort tests

--- src/test/java/pages/objects/ProductsPage.java
+++ src/test/java/pages/objects/ProductsPage.java
@@ -3,9 +3,11 @@ package pages.objects;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.ui.Select;
 import waits.WaitForElement;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProductsPage extends BasePage {
 
@@ -19,13 +21,16 @@ public class ProductsPage extends BasePage {
     List<WebElement> productItems;
 
     @FindBys({@FindBy(css = ".inventory_item_name")})
-    List<WebElement> productItemsName;
+    List<WebElement> productsName;
 
     @FindBys({@FindBy(css = ".inventory_item_price")})
     List<WebElement> productItemsPrice;
 
     @FindBy(css = ".shopping_cart_link")
     WebElement shoppingCartLink;
+
+    @FindBy(css = ".product_sort_container")
+    WebElement dropdownElement;
 
     public boolean isLogoDisplayed() {
         WaitForElement.waitForElementVisible(logo.get(0));
@@ -38,19 +43,30 @@ public class ProductsPage extends BasePage {
         return this;
     }
 
+    public ProductsPage setOrderPriceFromLoToHi() {
+        Select dropdown = new Select(dropdownElement);
+        dropdown.selectByValue("lohi");
+        return this;
+    }
+    public ProductsPage setOrderNameFromAToZ() {
+        Select dropdown = new Select(dropdownElement);
+        dropdown.selectByValue("az");
+        return this;
+    }
+
     public CartPage goToCartPage() {
         shoppingCartLink.click();
         return new CartPage();
     }
 
-    public Integer getProductItemsCount() {
+    public Integer getProductCount() {
         WaitForElement.waitUntilElementClickable(productItems.get(0));
         return productItems.size();
     }
 
-    public Integer getProductItemsNamesAndVerifyCharLength(int numberOfChars) {
-        WaitForElement.waitUntilElementClickable(productItemsName.get(0));
-        int count = (int) productItemsName.stream()
+    public Integer getProductNamesAndVerifyCharLength(int numberOfChars) {
+        WaitForElement.waitUntilElementClickable(productsName.get(0));
+        int count = (int) productsName.stream()
                 .map(WebElement::getText)
                 .filter(text -> text.length() > numberOfChars)
                 .count();
@@ -58,9 +74,9 @@ public class ProductsPage extends BasePage {
         return count;
     }
 
-    public Integer getProductItemsNamesAndVerifyNameContainText(String productName) {
-        WaitForElement.waitUntilElementClickable(productItemsName.get(0));
-        int count = (int) productItemsName.stream()
+    public Integer getProductNamesAndVerifyNameContainText(String productName) {
+        WaitForElement.waitUntilElementClickable(productsName.get(0));
+        int count = (int) productsName.stream()
                 .map(WebElement::getText)
                 .filter(text -> text.contains(productName))
                 .count();
@@ -68,8 +84,8 @@ public class ProductsPage extends BasePage {
         return count;
     }
 
-    public Integer getProductItemsPriceAndCheckIfValueEachOfThemIsGreaterThan(int priceOfProduct) {
-        WaitForElement.waitUntilElementClickable(productItemsName.get(0));
+    public Integer getProductPriceAndCheckIfValueEachOfThemIsGreaterThan(int priceOfProduct) {
+        WaitForElement.waitUntilElementClickable(productsName.get(0));
         int count = (int) productItemsPrice.stream()
                 .map(WebElement::getText)
                 .map(price -> price.replace("$", ""))
@@ -78,5 +94,33 @@ public class ProductsPage extends BasePage {
                 .count();
         log().info("Count number items where price is greater than " + priceOfProduct + " is: " + count);
         return count;
+    }
+
+    public boolean getProductPriceAndCheckIfNextProductHasHigherPrice() {
+        WaitForElement.waitUntilElementClickable(productsName.get(0));
+        List<Double> prices = productItemsPrice.stream()
+                        .map(product -> {
+                            String priceText = product.getText().replace("$", "");
+                            return Double.parseDouble(priceText);
+                        })
+                .collect(Collectors.toList());
+        double firstPrice = prices.get(0);
+        return prices.stream()
+                .skip(1)
+                .allMatch(price -> price >= firstPrice);
+    }
+
+    public boolean getProductNamesAndCheckIfIsSortFromAtoZ() {
+        WaitForElement.waitUntilElementClickable(productsName.get(0));
+        List<String> productNames = productsName.stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+
+        List<String> sortedProducts = productsName.stream()
+                .map(WebElement::getText)
+                .sorted()
+                .collect(Collectors.toList());
+
+        return productNames.equals(sortedProducts);
     }
 }
--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import pages.objects.LoginPage;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 import static utils.Helper.getLocalStorageSize;
 
 
@@ -36,7 +37,7 @@ public class ProductsTest extends TestBase {
     public void shouldSeeCorrectNumberOfProducts() {
         LoginPage loginPage = new LoginPage();
         Integer numberOfProducts = loginPage.logIn()
-                .getProductItemsCount();
+                .getProductCount();
 
         assertEquals(numberOfProducts, 6, "Incorrect number of products");
     }
@@ -50,27 +51,48 @@ public class ProductsTest extends TestBase {
     }
 
     @Test
-    public void shouldProductItemsNameContainMoreThan10Char() {
+    public void shouldProductNameContainMoreThan10Char() {
         LoginPage loginPage = new LoginPage();
-        int count = loginPage.logIn().getProductItemsNamesAndVerifyCharLength(10);
+        int count = loginPage.logIn().getProductNamesAndVerifyCharLength(10);
 
         assertEquals(count, 6, "Incorrect number of products whose name contains more than 10 characters");
     }
 
     @Test
-    public void shouldProductItemsNameContainTextSauce() {
+    public void shouldProductNameContainTextSauce() {
         LoginPage loginPage = new LoginPage();
-        int count = loginPage.logIn().getProductItemsNamesAndVerifyNameContainText("Sauce");
+        int count = loginPage.logIn().getProductNamesAndVerifyNameContainText("Sauce");
 
         assertEquals(count, 5, "Incorrect number of products whose name contains 'Sauce'");
     }
 
     @Test
-    public void shouldProductItemsPriceEachOfThemIsGreaterThan5Dollar() {
+    public void shouldProductPriceEachOfThemIsGreaterThan5Dollar() {
         LoginPage loginPage = new LoginPage();
-        int count = loginPage.logIn().getProductItemsPriceAndCheckIfValueEachOfThemIsGreaterThan(5);
+        int count = loginPage.logIn().getProductPriceAndCheckIfValueEachOfThemIsGreaterThan(5);
 
         assertEquals(count, 6, "Incorrect number of products whose price is grater than 5$");
+    }
+
+    @Test
+    public void shouldNextProductHasHigherPriceWhenOrderFromLoToHi() {
+        LoginPage loginPage = new LoginPage();
+        boolean isHigherPrice = loginPage.logIn()
+                .setOrderPriceFromLoToHi()
+                .getProductPriceAndCheckIfNextProductHasHigherPrice();
+
+        assertTrue(isHigherPrice, "Incorrect sorting by price from lowest to highest");
+    }
+
+
+    @Test
+    public void shouldSortProductsNamesFromAtoZ() {
+        LoginPage loginPage = new LoginPage();
+        boolean isHigherPrice = loginPage.logIn()
+                .setOrderNameFromAToZ()
+                .getProductNamesAndCheckIfIsSortFromAtoZ();
+
+        assertTrue(isHigherPrice, "Incorrect sorting by name from A to Z");
     }
 
 }
